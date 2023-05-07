766097b8ea96a0266f11ff3f625ec0589a190e6b
Bob Honores <bobhonores@gmail.com>
2023-05-07 00:21:37 -0500

    feat: implementing getaction and addaction methods

--- internal/transport/grpc/action.go
+++ internal/transport/grpc/action.go
@@ -2,18 +2,52 @@ package grpc
 
 import (
 	"context"
+	"errors"
+	"log"
 
 	api "github.com/bobhonores/planner/api/v1"
+	"github.com/bobhonores/planner/internal/action"
+	"github.com/google/uuid"
 )
 
 func (h *Handler) GetAction(ctx context.Context, req *api.GetActionRequest) (*api.GetActionResponse, error) {
-	// actionID, err := uuid
-	// h.ActionService.GetByID(ctx, )
-	return &api.GetActionResponse{}, nil
+	actionID, err := uuid.Parse(req.GetId())
+	if err != nil {
+		return nil, errors.New("incorrect id format")
+	}
+	action, err := h.ActionService.GetByID(ctx, actionID)
+	if err != nil {
+		return nil, errors.New("action not found")
+	}
+	return &api.GetActionResponse{
+		Action: &api.Action{
+			Id:          action.ID.String(),
+			Name:        action.Name,
+			Description: action.Description,
+			Done:        action.Done,
+		},
+	}, nil
 }
 
 func (h *Handler) AddAction(ctx context.Context, req *api.AddActionRequest) (*api.AddActionResponse, error) {
-	return &api.AddActionResponse{}, nil
+	action, err := h.ActionService.Insert(ctx, action.Action{
+		Name:        req.Name,
+		Description: req.Description,
+		Done:        req.Done,
+	})
+	if err != nil {
+		log.Println(err)
+		return nil, errors.New("cannot insert the action")
+	}
+
+	return &api.AddActionResponse{
+		Action: &api.Action{
+			Id:          action.ID.String(),
+			Name:        action.Name,
+			Description: action.Description,
+			Done:        action.Done,
+		},
+	}, nil
 }
 
 func (h *Handler) UpdateAction(ctx context.Context, req *api.UpdateActionRequest) (*api.UpdateActionResponse, error) {
