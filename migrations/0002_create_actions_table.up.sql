7278534d04bdff508b54005cf220810b8dfc4804
Bob Honores <bobhonores@gmail.com>
2023-05-07 00:20:53 -0500

    fix: chaging data type of actions table

--- migrations/0002_create_actions_table.up.sql
+++ migrations/0002_create_actions_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS actions (
     id              uuid PRIMARY KEY,
-    name          varchar(500) NOT NULL,
-    description   text NULL,
-    done            bit NOT NULL,
-    created         timestamp with time zone NOT NULL,
+    name            varchar(500) NOT NULL,
+    description     text NULL,
+    done            boolean NOT NULL,
+    created         timestamp with time zone NULL,
     updated         timestamp with time zone NULL
 );
