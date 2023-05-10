7f8c52daf887663f0db43345898be7444cd3e825
Bob Honores <bobhonores@gmail.com>
2023-05-10 00:56:20 -0500

    fix: fixing status code on action test

--- tests/action_test.go
+++ tests/action_test.go
@@ -9,6 +9,8 @@ import (
 	v1 "github.com/bobhonores/planner/api/v1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type ActionTestSuite struct {
@@ -30,6 +32,36 @@ func (s *ActionTestSuite) TestAddAction() {
 		assert.NotEmpty(s.T(), resp.Action.Id)
 		assert.Equal(s.T(), "do your homework", resp.Action.Name)
 		assert.Equal(s.T(), "this activity should be made by 10 pm", resp.Action.Description)
+	})
+}
+
+func (s *ActionTestSuite) TestGetAction() {
+	s.T().Run("gets an invalid argument", func(t *testing.T) {
+		client := GetClient()
+		_, err := client.GetAction(
+			context.Background(),
+			&v1.GetActionRequest{
+				Id: "invalid-id",
+			},
+		)
+		assert.Error(s.T(), err)
+
+		st := status.Convert(err)
+		assert.Equal(s.T(), codes.InvalidArgument, st.Code())
+	})
+
+	s.T().Run("gets a non-found action", func(t *testing.T) {
+		client := GetClient()
+		_, err := client.GetAction(
+			context.Background(),
+			&v1.GetActionRequest{
+				Id: "b304da2d-77b4-491c-a532-2ce17c0eceef",
+			},
+		)
+		assert.Error(s.T(), err)
+
+		st := status.Convert(err)
+		assert.Equal(s.T(), codes.NotFound, st.Code())
 	})
 }
 
